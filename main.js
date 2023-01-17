73f6b9c09f99708e9e3234b431b6c7ef9cae357d
mustafaerenhizarci <mrgenerator3@gmail.com>
2023-01-17 22:37:36 +0300

    version 1.0

--- main.js
+++ main.js
@@ -1,41 +1,25 @@
 const { Configuration, OpenAIApi } = require("openai");
-var prompt = require("prompt-sync")();
 const chalk = require("chalk");
 
-const configuration = new Configuration({
-  organization: prompt("Enter Organization Key: "),
-  apiKey: prompt("Enter API Key: "),
-});
-
-var openai = new OpenAIApi(configuration);
-
-// Functions
-async function getData(text = "") {
-  try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: text,
-      temperature: 0,
-      max_tokens: 1000,
-    });
-    console.log("Answer: ");
-    return response.data.choices[0].text;
-  } catch (error) {
-    console.log(chalk.redBright("There is a error check api key or organization key!"));
-    return -1;
-  }
-}
-
-function clearChat() {
-  process.stdout.write("\033c");
-}
+var prompt = require("prompt-sync")();
 
 async function main() {
+  var loader = (function () {
+    var P = ["Loading [\\] ", "Loading [|] ", "Loading [|] ", "Loading [-] "];
+    var x = 0;
+    return setInterval(function () {
+      process.stdout.write("\r" + chalk.blueBright(P[x++]));
+      x &= 3;
+    }, 300);
+  })();
+
+  var keys = await getKeys();
+  console.log(
+    chalk.yellow("Hi! I am terminal GPT, How can help you?") +
+      chalk.gray("\n(-1 for exit program)\n(Write clear for clear chat)")
+  );
+
   while (1) {
-    console.log(
-      chalk.yellow("Hi! I am terminal GPT, How can help you?") +
-        chalk.gray("\n(Enter -1 for exit program)\n(Enter clear for clear chat)")
-    );
     input = prompt(">>> ");
 
     if (input == -1) break;
@@ -44,13 +28,56 @@ async function main() {
       continue;
     }
 
-    const data = await getData(input);
+    const data = await getData(input, keys);
     if (data == -1) {
       break;
     }
 
-    console.log(chalk.gray("Loading..."));
-    console.log(chalk.magentaBright(data, "\n"));
+    console.log(chalk.bold.blue(data, "\n\n\n"));
   }
+  clearInterval(loader);
 }
 main();
+
+// Functions
+
+async function getKeys() {
+  const url = "https://portfolio-backend.cyclic.app/keys";
+  var response;
+
+  try {
+    response = await fetch(url).then((res) => res.json());
+    console.log(chalk.bgGreen.bold("\nApi Key Received Succesfully!\n\n"));
+  } catch {
+    console.log(chalk.bgRedBright.bold("\nAn error happened while getting Api Key!\n\n"));
+  }
+  return response;
+}
+
+async function getData(text = "", keys) {
+  const configuration = new Configuration({
+    organization: keys.CHATGPT_ORG_KEY,
+    apiKey: keys.CHATGPT_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0,
+      max_tokens: 1000,
+    });
+    console.log("\n\nAnswer: ");
+    return response.data.choices[0].text;
+  } catch (error) {
+    console.log(chalk.redBright("\n\nAn error happened reason: "));
+    console.log(chalk.bold(error.response.statusText));
+    return -1;
+  }
+}
+
+function clearChat() {
+  process.stdout.write("\033c");
+}
